d7b1854af73469b9974ab1d807af8ba50ba6928a
Will J <willj.94@gmail.com>
2018-05-20 19:11:48 -0600

    chore: init VirtuanceWP from the test base

---
+++ VirtuanceWP/wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'wG1Bwx0TJ7CshGKQ8S4425q5oNzDDM3oArizYRXfYmF1mqdVwA3x39EjAfa9oXyW');
+define('SECURE_AUTH_KEY',  'es6YI78XgtbvwfWEaeM6w3gy5srR1xSfXp0MmiLEiN5XsNCy4nzU3a5VLW0GbVyC');
+define('LOGGED_IN_KEY',    'IjW19mFvRb3HXFxCy2ovJB7yLcigXMbCNQvJusyAHGq5DxqG07nVT8ULiC58JSgn');
+define('NONCE_KEY',        'eMPTMAUMK126dYJgUgNcfsLYfJMAzyG9RrHHDsKKyn9r3PgLvGYJrxnQTNsjhsW1');
+define('AUTH_SALT',        '0AbUgVfAMdw2fIJ2XmPmNNnfgKbb1Uw491GRCecMGr7SqQF8vfPpgYvM4xdK77ch');
+define('SECURE_AUTH_SALT', 'Xmd9GgHXi9FEbCc6G00qHSywDWXVuFfaynLahEDit5TJcJWn3HodY3TVA2p7wMF9');
+define('LOGGED_IN_SALT',   'NA21LMSpGYEeK9XMxFcdWLbosWYiCP2BtPp23T2VK3FECb9UqP1CRI8B54sz2f62');
+define('NONCE_SALT',       'LdRaQocccgXcu8Rs1MfE5DpoFhyRbvCzNAgdT8wiPRPwMrhbCgwT3n9c5Gw4dUFQ');
